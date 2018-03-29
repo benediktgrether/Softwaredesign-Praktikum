d594533191b7ebd2fa4f35579da6f1250a3d0b0d
Benedikt Grether <benedikt@pixelarbeit.de>
2018-03-29 16:26:17 +0200

    Work on if - Random

--- 1-2-aufgabe/Program.cs
+++ 1-2-aufgabe/Program.cs
@@ -4,22 +4,53 @@ namespace _1_2_aufgabe
 {
     class Program
     {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            GetVerse();
-            // static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
-            // static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
-            // static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+            int num;
+            int[] array = new int[5];
+            int[] count = new int[5];
+            for (int i = 0; i < count.Length; i++)
+            {
+                num = GetVerse();
+                count[i] = num;
+
+                // num = GetVerse();
+                // // count[i] = num;
+                // int numTwo = GetVerse();
+                // array[i] = numTwo;
+                // // Console.Write(num + " | ");
+                // if( num == array[i]){
+                //     count[i] = num;
+                //     Console.WriteLine( num + " == " + count[i] + " -- " + array[i] );
+                // }else{
+                //     Console.WriteLine( num + " != " + count[i] + " -- " + array[i] );
+                //     num = GetVerse();
+                //     // array[i] = num;
+                // }
+
+                // if (count[i] == num){
+                //     num = GetVerse();
+                //     // count[i] = num;
+                // }else{
+                //     count[i] = num;
+                //     // num = GetVerse();
+                // }
+                
+                // Console.WriteLine( i + " Random number is " + num + " " + subjects[num]);
+                // Console.WriteLine( "Zähler :" + count[i]);
+            }
+            // Console.WriteLine("Random number is " + num);
         }
-    // public static void GetVerse(string[] subjects, string[] verbs, string[] objects){
 
-    // }
-
-        public static void GetVerse(){
+        public static int GetVerse(){
             Random rnd = new Random();
             int num = rnd.Next(1, 5);
-            Console.WriteLine(num);
+            // Console.WriteLine(num);
+            return num;
         }
     }
 }
