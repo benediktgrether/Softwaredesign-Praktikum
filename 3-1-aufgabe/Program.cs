930f7028e4344077a41cb932242d6da37a21d586
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-17 12:39:48 +0200

    Add new Methode

--- 3-1-aufgabe/Program.cs
+++ 3-1-aufgabe/Program.cs
@@ -7,6 +7,12 @@ namespace _3_1_aufgabe
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int value = Convert.ToInt32(args[0]);
+            toHexal(value);
+            }
+        public static int toHexal(int value)
+        {
+            return value;
         }
     }
 }
