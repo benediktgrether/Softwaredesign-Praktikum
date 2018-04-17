22b05bdc8f61a7828efc17f2c101e60001148118
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-17 11:32:13 +0200

    Add new folder to Repo

---
+++ 3-1-aufgabe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_1_aufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
