b9290eb2898d7247ef4b23ea076d5da8b5d1b011
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-18 09:00:09 +0200

    Add comments

--- 3-1-aufgabe/Program.cs
+++ 3-1-aufgabe/Program.cs
@@ -8,10 +8,24 @@ namespace _3_1_aufgabe
         {
             Console.WriteLine("Hello World!");
             int value = Convert.ToInt32(args[0]);
+            
+            // ToDo - If Bediengung erstellen das den Wert zwischen 0 und 1023 überprüft   
+            
             toHexal(value);
             }
         public static int toHexal(int value)
         {
+            /* 
+            Umwandlung von 6 --> Dez
+            Die Basis ist hier 6. 
+            Jede Zahl in diesem System wird als Summe von Potenzen in 6 geschrieben, 
+            die Faktoren vor diesen Potenzen sind die Ziffern der Zahl in diesem System: 
+            1324(6)=1⋅6^3+3⋅6^2+2⋅6^1+4
+            */
+
+            // To-Do int -> Länge zählen und eine for-schleife rückwärtszählen 
+            // int zahl auseinander nehmen
+
             return value;
         }
     }
