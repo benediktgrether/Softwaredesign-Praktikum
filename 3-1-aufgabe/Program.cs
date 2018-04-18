6012b1fd5f0679468919906f1f64e56796e15100
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-18 12:45:54 +0200

    Work on array

--- 3-1-aufgabe/Program.cs
+++ 3-1-aufgabe/Program.cs
@@ -6,34 +6,34 @@ namespace _3_1_aufgabe
     {
         static void Main(string[] args)
         {
-            int value = Convert.ToInt32(args[0]);
+            Console.WriteLine(toHexal(Convert.ToInt32(args[0])));
+            // int value = Convert.ToInt32(args[0]);
             
-            if (0 <= value && value <= 1023 ){
-                toHexal(value);
-            }else{
-                Console.WriteLine("Please make a input from 0 to 1023");
-            }
+            // if (0 <= value && value <= 1023 ){
+                // toHexal(value);
+            // }else{
+            //     Console.WriteLine("Please make a input from 0 to 1023");
+            // }
         }
         public static int toHexal(int value)
         {
 
-            Console.WriteLine(value + " Der Rest beträgt : " + value % 6);
-            for (int i = value.ToString().Length; i > 0; i--)
+            int newvalue ;
+            int rest;
+            int[] arr = new int[value.ToString().Length + 1];
+            
+            for (int i = 0; i <= value.ToString().Length + 1; i++)
             {
-                Console.WriteLine(i);
-            }
-           
-            /* 
-            Umwandlung von 6 --> Dez
-            Die Basis ist hier 6. 
-            Jede Zahl in diesem System wird als Summe von Potenzen in 6 geschrieben, 
-            die Faktoren vor diesen Potenzen sind die Ziffern der Zahl in diesem System: 
-            1324(6)=1⋅6^3+3⋅6^2+2⋅6^1+4
-            */
-
-            // To-Do int -> Länge zählen und eine for-schleife rückwärtszählen 
-            // int zahl auseinander nehmen
-
+                newvalue = value / 6;
+                rest = value % 6;
+                arr[i] = rest;
+                Console.WriteLine(newvalue + " mod " + rest );
+                Console.WriteLine("array test " + arr[i]);
+                value = newvalue;
+            } 
+            Array.Reverse(arr);
+            // return arrayrest;
+            Console.WriteLine("Debugger " + string.Join("", arr));
             return value;
         }
     }
