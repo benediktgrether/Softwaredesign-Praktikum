df0512eadca3b49e1ba55fc71da8bf86a9d050c1
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-18 16:31:08 +0200

    Add Convert to decimal form Base

--- 3-1-aufgabe/Program.cs
+++ 3-1-aufgabe/Program.cs
@@ -7,20 +7,22 @@ namespace _3_1_aufgabe
         static void Main(string[] args)
         {
             // Console.WriteLine(ConvertDecimalToHexal(Convert.ToInt32(args[0])));
-            Console.WriteLine(ConvertHexalToDezimal(Convert.ToInt32(args[0])));
+            // Console.WriteLine(ConvertHexalToDezimal(Convert.ToInt32(args[0])));
+            // Console.WriteLine(ConvertToBaseFromDecimal(Convert.ToInt32(args[0]), Convert.ToInt32(args[1])));
+            Console.WriteLine(ConvertToDecimalFromBase(Convert.ToInt32(args[0]), Convert.ToInt32(args[1])));
         }
         public static int ConvertDecimalToHexal(int dec)
         {
             //To Do - if input from 0 to 1023
             int value ;
-            int rest;
+            int modulo;
             int[] arr = new int[4];
             
             for (int i = 0; i <= dec.ToString().Length + 2; i++)
             {
                 value = dec / 6;
-                rest = dec % 6;
-                arr[i] = rest;
+                modulo = dec % 6;
+                arr[i] = modulo;
                 dec = value;
             } 
             Array.Reverse(arr);
@@ -55,5 +57,46 @@ namespace _3_1_aufgabe
             }
             return sum;
         }
+
+        public static int ConvertToBaseFromDecimal(int toBase, int dec)
+        {
+            int value;
+            int modulo;
+            int[] arr = new int[4];
+            
+            for (int i = 0; i <= dec.ToString().Length + 2; i++)
+            {
+                value = dec / toBase;
+                modulo = dec % toBase;
+                arr[i] = modulo;
+                dec = value;
+            } 
+            Array.Reverse(arr);
+            int newArr = Convert.ToInt32((string.Join("", arr)));
+            return newArr;
+        }
+        public static int ConvertToDecimalFromBase(int fromBase, int number)
+        {
+            int a = Math.Abs(number);
+            int length = a.ToString().Length;
+            int[] array = new int[length];
+
+            // new array
+            int [] arr = new int[length];
+            int sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = a % 10;
+                a /= 10;
+                arr[i] += array[i] * Convert.ToInt32(Math.Pow(fromBase, i));
+            }
+            for (int j = 0; j < arr.Length; j++)
+            {
+                sum += arr[j];
+            }
+            return sum;
+        }
+
     }
 }
