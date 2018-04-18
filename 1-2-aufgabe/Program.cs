7eb209fc0d04f706b70c9931001e97be6432fea1
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-18 16:32:05 +0200

    Changes 1-2-Aufgabe

--- 1-2-aufgabe/Program.cs
+++ 1-2-aufgabe/Program.cs
@@ -4,11 +4,11 @@ namespace _1_2_aufgabe
 {
     class Program
     {
-        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
-        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
-        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        static string[] Subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] Verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] Objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
 
-        static int subLength = subjects.Length;
+        static int subLength = Subjects.Length;
         static string Sub;
         static string Verb;
         static string Obj;
@@ -16,12 +16,12 @@ namespace _1_2_aufgabe
         static void Main(string[] args)
         {
             string[] verse = new string[subLength];
-            for (int i = 0; i < subjects.Length; i++)
+            for (int i = 0; i < Subjects.Length; i++)
             {
                 GetVerse();
                 verse[i] = Sub + " " + Verb + " " + Obj;
             }
-            for (int i = 0; i < subjects.Length; i++){
+            for (int i = 0; i < Subjects.Length; i++){
                 Console.WriteLine(verse[i]);
             }
         }
@@ -33,26 +33,26 @@ namespace _1_2_aufgabe
             int verbNum = rnd.Next(0, subLength);
             int objNum = rnd.Next(0, subLength);
 
-            while (subjects[subNum] == "used")
+            while (Subjects[subNum] == "used")
             {
                 subNum = rnd.Next(0, subLength);
             }
-            Sub = subjects[subNum];
-            subjects[subNum] = "used";
+            Sub = Subjects[subNum];
+            Subjects[subNum] = "used";
 
-            while (verbs[verbNum] == "used")
+            while (Verbs[verbNum] == "used")
             {
                 verbNum = rnd.Next(0, subLength);
             }
-            Verb = verbs[verbNum];
-            verbs[verbNum] = "used";
+            Verb = Verbs[verbNum];
+            Verbs[verbNum] = "used";
 
-            while (objects[objNum] == "used")
+            while (Objects[objNum] == "used")
             {
                 objNum = rnd.Next(0, subLength);
             }
-            Obj = objects[objNum];
-            objects[objNum] = "used";
+            Obj = Objects[objNum];
+            Objects[objNum] = "used";
         }
     }
 }
