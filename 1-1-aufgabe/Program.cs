a9cc643178191060653387b33d8559b9f0b1446e
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-18 10:56:15 +0200

    refactoring to clean-code

--- 1-1-aufgabe/Program.cs
+++ 1-1-aufgabe/Program.cs
@@ -6,68 +6,56 @@ namespace _1_1_aufgabe
     {
         static void Main(string[] args)
         {
-            string w = "w";
-            string k = "k";
-            string o = "o";
-            var eingabe = (args[0]);
-            var Num = (args[1]); 
-            double d = Convert.ToDouble(Num);
-             if (w == eingabe){
-                getCubeInfo(d);
-            }else if( k == eingabe ){
-                getSphereInfo(d);
-            }else if( o == eingabe ){
-                getSphereInfo(d);
-            }else{
-                Console.WriteLine("Wrong input");
-            }            
+            double length = Convert.ToDouble(args[1]);
+            
+            switch(args[0])
+            {
+                case "w":
+                    Console.WriteLine(getCubeInfo(length));
+                    break;
+                case "k":
+                    Console.WriteLine(getSphereInfo(length));
+                    break;
+                case "o":
+                    Console.WriteLine(getOctahedronInfo(length));
+                    break;
+                
+                default:
+                    Console.WriteLine("Please make an input between W, K, O");
+                    break;
+            }           
         }
         // Cube
-        public static double getCubeSurface(double durchmesser){
-            double d = durchmesser;
-            double a = Math.Round(6 * (d * d), 2);
-            return a;
+        public static double getCubeSurface(double length){
+            return Math.Round(6 * (length * length), 2);
         }
-        public static double getCubeVolume(double durchmesser){
-            double d = durchmesser;
-            double v = Math.Round(d * d * d, 2);
-            return v;
+        public static double getCubeVolume(double length){
+            return Math.Round(length * length * length, 2);
         }
-        public static void getCubeInfo(double durchmesser){
-            double d = durchmesser;
-            Console.WriteLine("Kubus: A=" + getCubeSurface(d) + " | V=" + getCubeVolume(d) );
+        public static string getCubeInfo(double length){
+            return "Kubus: A = " + getCubeSurface(length) + " | V = " + getCubeVolume(length);
         }
 
         // Sphere
-        public static double getSphereSurface(double durchmesser){
-            double d = durchmesser;
-            double a = Math.Round(Math.PI * ( d * d), 2);
-            return a;
+        public static double getSphereSurface(double length){
+            return Math.Round(Math.PI * ( length * length), 2);
         }
-        public static double getSphereVolume(double durchmesser){
-            double d = durchmesser;
-            double v = Math.Round(Math.PI * ( d * d * d) / 6 , 2);
-            return v;
+        public static double getSphereVolume(double length){
+            return Math.Round(Math.PI * ( length * length * length) / 6 , 2);
         }
-        public static void getSphereInfo(double durchmesser){
-            double d = durchmesser;
-            Console.WriteLine("Kugel: A=" + getSphereSurface(d) + " | V=" + getSphereVolume(d) );
+        public static string getSphereInfo(double length){
+            return "Kugel: A = " +  getSphereSurface(length) + " | V = " + getSphereVolume(length);
         }
         
         // Octahedron
-        public static double getOctahedronSurface(double durchmesser){
-            double d = durchmesser;
-            double a = Math.Round(2 *( Math.Sqrt(3)) * d * d, 2);
-            return a;
+        public static double getOctahedronSurface(double length){
+            return Math.Round(2 *( Math.Sqrt(3)) * length * length, 2);
         }
-        public static double getOctahedronVolume(double durchmesser){
-            double d = durchmesser;
-            double v = Math.Round(Math.Sqrt(2) * d * d * d / 3 , 2);
-            return v;
+        public static double getOctahedronVolume(double length){
+            return Math.Round(Math.Sqrt(2) * length * length * length / 3 , 2);
         }
-        public static void getOctahedronInfo(double durchmesser){
-            double d = durchmesser;
-            Console.WriteLine("Oktaeder: A=" +getOctahedronSurface(d) + " | V=" + getOctahedronVolume(d) );
+        public static string getOctahedronInfo(double length){
+            return "Oktaeder: A = " +getOctahedronSurface(length) + " | V = " + getOctahedronVolume(length);
         }
     }
 
