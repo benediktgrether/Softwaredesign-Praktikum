7521b2ecafc4656dfbbf933f002931c991bfb0e4
Benedikt Grether <benedikt@pixelarbeit.de>
2018-04-25 09:25:46 +0200

    Add new Folder

---
+++ 4-1-aufgabe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_1_aufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
