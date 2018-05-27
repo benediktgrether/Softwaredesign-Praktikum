147bf46e13a198455ee53f75791e814ee2dfe79b
Benedikt Grether <benedikt@pixelarbeit.de>
2018-05-27 14:32:51 +0200

    Changes in Class

--- uml-aufgabe/Program.cs
+++ uml-aufgabe/Program.cs
@@ -1,36 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace uml_aufgabe
 {
-    interface Person
+    public class CoursesList<T>
     {
-        string FirstName();
-        string LastName();
-        int Age();
-    }
 
-    interface Member : Person
-    {
-        int MatriculationNumber();
-        string[] courses();
-    }
+        class Person
+        {
+            public string FirstName;
+            public string LastName;
+            public int Age;
+        }
 
-    interface Professor : Person 
-    {
-        string Room();
-        DateTime OfficeDays();
-        DateTime OfficeHours();
-        string[] courses();
-    }
+        class Member : Person
+        {
+            public int MatriculationNumber;
+            public List<CoursesList<T>> course = new List<CoursesList<T>>();
+        }
 
-    interface Course : Professor, Member
-    {
-        string Titel();
-        DateTime day();
-        DateTime time();
-        string CoursRoom();
-    }
+        class Professor : Person 
+        {
+            public string Room;
+            public DateTime OfficeDays;
+            public DateTime OfficeHours;
+            public List<CoursesList<T>> course = new List<CoursesList<T>>();
 
+            public void OutputCourse(){
+                foreach (T course in Course)
+                {
+                    Console.WriteLine(Course.Title);
+                }
+            }
+            public Participiant<T> OutputTotalParticipant()
+            {
+                List<Participiant> TotalParticipiant = new List<Participiant>();
+                foreach (T course in Course)
+                {
+                    foreach(Participiant participiant in Member.Participiant)
+                    {
+                        if(!TotalParticipiant.Contains(participiant))
+                        {
+                            TotalParticipant.Add(participiant);
+                        }
+                    }
+                }
+                return TotalParticipant;
+            }
+
+
+        }
+
+        class Course 
+        {
+
+            public string Titel;
+            public DateTime Day;
+            public DateTime Time;
+            public string CourseRoom;
+
+            public Professor Professor;
+            List<Participiant> TotalParticipiant;
+            public void OutputInformation(){
+                Console.WriteLine("Kurs: " + Titel + " Tag: " + Day + " Uhrzeit:  " + Time + " Raum: " + CourseRoom);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
