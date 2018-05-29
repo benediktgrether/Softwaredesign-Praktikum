5a4c92f56ffc0d770523412d62628c0b461a8cf2
Benedikt Grether <benedikt@pixelarbeit.de>
2018-05-29 12:12:42 +0200

    Bugfixes

--- uml-aufgabe/Program.cs
+++ uml-aufgabe/Program.cs
@@ -3,65 +3,58 @@ using System.Collections.Generic;
 
 namespace uml_aufgabe
 {
-    public class CoursesList<T>
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+    }
+
+    class Participant : Person
+    {
+        public int MatriculationNumber;
+        public List<Course> Course;
+    }
+
+    class Professor : Person 
+    {
+        public string Room;
+        public DateTime OfficeDays;
+        public DateTime OfficeHours;
+
+        public List<Course> Course;
+
+        public void OutputCourse()
+        {
+            foreach (Course course in Course)
+                Console.WriteLine(course.Titel);
+        }
+
+        public List<Participant> OutputTotalParticipant()
+        {
+            List<Participant> TotalParticipant = new List<Participant>();
+            
+            foreach (Course course in Course)
+                foreach(Participant participant in course.Participant)
+                    if(!TotalParticipant.Contains(participant))
+                        TotalParticipant.Add(participant);
+
+            return TotalParticipant;
+        }
+    }
+
+    class Course 
     {
 
-        class Person
-        {
-            public string FirstName;
-            public string LastName;
-            public int Age;
-        }
+        public string Titel;
+        public DateTime Day;
+        public DateTime Time;
+        public string CourseRoom;
 
-        class Member : Person
-        {
-            public int MatriculationNumber;
-            public List<CoursesList<T>> course = new List<CoursesList<T>>();
-        }
-
-        class Professor : Person 
-        {
-            public string Room;
-            public DateTime OfficeDays;
-            public DateTime OfficeHours;
-            public List<CoursesList<T>> course = new List<CoursesList<T>>();
-
-            public void OutputCourse(){
-                foreach (T course in Course)
-                {
-                    Console.WriteLine(Course.Title);
-                }
-            }
-            public Participiant<T> OutputTotalParticipant()
-            {
-                List<Participiant> TotalParticipiant = new List<Participiant>();
-                foreach (T course in Course)
-                {
-                    foreach(Participiant participiant in Member.Participiant)
-                    {
-                        if(!TotalParticipiant.Contains(participiant))
-                        {
-                            TotalParticipant.Add(participiant);
-                        }
-                    }
-                }
-                return TotalParticipant;
-            }
-        }
-
-        class Course 
-        {
-
-            public string Titel;
-            public DateTime Day;
-            public DateTime Time;
-            public string CourseRoom;
-
-            public Professor Professor;
-            List<Participiant> TotalParticipiant;
-            public void OutputInformation(){
-                Console.WriteLine("Kurs: " + Titel + " Tag: " + Day + " Uhrzeit:  " + Time + " Raum: " + CourseRoom);
-            }
+        public Professor Professor;
+        public List<Participant> TotalParticipant;
+        public void OutputInformation(){
+            Console.WriteLine("Kurs: " + Titel + " Tag: " + Day + " Uhrzeit:  " + Time + " Raum: " + CourseRoom);
         }
     }
     
